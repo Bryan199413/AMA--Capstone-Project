2e6c8714bdf5c9defa138461a9918f4f0d90fe5a
brynlcntr13 <brynlcntr13@gmail.com>
2024-03-08 14:35:03 +0800

    Added changes for Sidenav component

--- client/src/Components/SideNav.jsx
+++ client/src/Components/SideNav.jsx
@@ -5,19 +5,64 @@ import useLogout from '../hooks/useLogout';
 
 function SideNav() {
   const {logout} = useLogout()
-  
+  const [tab,setTab] = useState('Chats')
+
   const storedData = localStorage.getItem("machimachi-user");
   const userData = JSON.parse(storedData);
   const avatarURL = userData.avatar;
+
+  const handleTab = (tab) => {
+    setTab(tab)  
+  }
+  
+  
   return (
    <div className='flex flex-col w-80 h-full bg-base-200 p-2'>
-        
-       <div className='text-md flex justify-between rounded-lg bg-base-300 p-1'>
-          <button className='flex-1 py-2 rounded-lg focus:bg-base-100'>Chats</button>
-          <button className='flex-1 py-2 rounded-lg focus:bg-base-100'>Friends</button>
-       </div>
+       <div className='h-full '>
+          <div className='text-md flex justify-between rounded-lg bg-base-300 p-1'>
+            <button className={tab === 'Chats' ? 'flex-1 py-2 rounded-lg bg-base-100' : 'flex-1 py-2 rounded-lg'} onClick={() => handleTab('Chats')}>Chats</button>
+            <button className={tab === 'Friends' ? 'flex-1 py-2 rounded-lg bg-base-100' : 'flex-1 py-2 rounded-lg'} onClick={() => handleTab('Friends')}>Friends</button>
+          </div>
+           
+            <div className={tab === 'Chats' ? 'block overflow-auto' : 'hidden'}>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer '>Talk to strangers</div>
+               <div className="divider">Direct Messages</div>
+
+               <div className=' h-[650px]'>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+               <div className='text-center text-[#41B8D5] p-2 bg-base-300 my-1 rounded-lg cursor-pointer'>Talk to strangers</div>
+             
+            </div>
+           
+     
+          
+              
+             
+               
+            </div>
+
+            <div className={tab === 'Friends' ? 'block text-center' : 'hidden'}>Friends</div>
+
+           
+        </div>
+      
    
-         <div className='h-[100%] text-center'>Messages & friends list</div>
+       
                
         
          <div className='p-3 flex justify-between items-center hover:bg-base-100'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,17 +5,16 @@ import SideNav from '../Components/SideNav'
 function Home() {
   return (
     <div className='bg-base-300 h-screen flex flex-col'>
-        <Navbar/>
-       
-        <div className='w-full  h-full flex'>
-           <div className='hidden lg:block'>
-           <SideNav />
-           </div>
-            
-           <div className='text-center w-full'>
-           chatbox here!
-           </div>
+      <Navbar/>
+      <div className='w-full h-full flex'>
+        <div className='hidden lg:block'>
+          <SideNav />
         </div>
+        
+        <div className='text-center w-full'>
+            chatbox here!
+        </div>
+      </div>
         
        
     </div>
