bf073cba9784ce77e919a07a1c2d06ec4ceb0fa5
Bryan Alcantara <Brynlcntr13>
2024-05-12 21:33:06 +0800

    add update for deployment

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use('/api/friends',friendRoutes);
 app.use(express.static(path.join(__dirname,"../client/dist")));
 
 app.get("*",(req,res) => {
-    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+    res.sendFile(path.join(__dirname,"../client","dist","index.html"))
 })
 
 server.listen(PORT, () => {
