0bda9d5f72fc94ba2d78cee270bb0cb6598bce70
Bryan Alcantara <Brynlcntr13>
2024-05-13 00:36:03 +0800

    update socket

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:5000,
     proxy:{
       "/api":{
-        target:"https://machimachi.onrender.com",
+        target:"http://localhost:4001",
       }
     }
   },
